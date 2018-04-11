fa8e9854b686223e60cabec31f99b46d3c295210
joeslzr <38015581+joeslzr@users.noreply.github.com>
2018-04-10 22:04:41 -0600

    Add files via upload

--- workout_routine.php
+++ workout_routine.php
@@ -2,61 +2,104 @@
     include_once 'header.php';
     include_once 'includes/dbh.inc.php'
 ?>
-
+ 
 <section class ="main-container">
     <div class="main-wrapper">
         <?php
             if(isset($_SESSION['u_uid'])){
-				$firstname = $_SESSION['u_first'];
+                $firstname = $_SESSION['u_first'];
                 $id = $_SESSION['u_id'];
-				$sql = "SELECT * FROM workoutroutine WHERE UserID = $id";
+                $sql = "SELECT * FROM workoutroutine WHERE UserID = $id";
                 $result = mysqli_query($conn,$sql) or die("Bad Query: $sql");
-                
-                echo "<h2>$firstname's Workout Routine's</h2>";
-                
+                // if 
+                echo "<h2>$firstname's Routines</h2>";
+                 
                 echo '<form id="form" action="" method="post">';
-                echo '<select name="workout" class="ddList">';
-                echo "<option value='default'>Please Select a Plan</option>";
+                echo '<select name="routine" class="ddList">';
+                echo "<option value='default'>Please Select a Routine</option>";
                 echo "<option value='new'>Create New</option>";
                 while($row = mysqli_fetch_assoc($result)){
                     $rowname = $row['Name'];
-                    $rowvalue = $row['WorkoutID'];
-                    echo "<option value='$rowvalue'>" . $rowname . "</option>";
+                    echo "<option value='$rowname'>" . $rowname . "</option>";
                 }
                 echo '</select>';
                 echo '<input type="submit" value="Select">';
                 echo '</form>';
-                
-                $value = $_POST['workout'] ?? 'default';
-                                
+                $value = $_POST['routine'] ?? 'default';
+                                 
                 if($value == "default"){
                 }else if($value == "new"){
-                    echo "TO BE ADDED.";
+					//Create new Routine
+                     echo '<form class="workoutroutine-form" action="includes/workout_routine.inc.php" method="POST"> 
+                                <h2>New Routine</h2>
+                                <input type ="text" name ="name" placeholder="Routine Name">
+                                <button type ="submit" name="submit_routine">Create</button>
+                                </form>';
                 }else{
-                    echo "<h2>Workout Details for Plan:</h2>";
-                    echo "<h2>$rowname</h2>";
+					//get workoutID
+					$sql = "SELECT * FROM workoutRoutine WHERE Name = '$value' AND UserID = $id"; 
+					$result = mysqli_query($conn, $sql) or die("Bad Query: $sql");
+					$row = mysqli_fetch_assoc($result);
+					$w_id = $row['WorkoutID'];
+										
+                    // Shows routine info
+                    echo "<h2>Routine Breakdown:</h2>";
+                    echo "<h2>$value</h2>";
                     echo "<table border='1'>";
-                    echo "<tr><td>Excercises</td><td>Targeted Body Part</td><td>Sets</td><td>Reps</td></tr>";
-                    $sql = "SELECT e.name, e.bodyPart, c.sets, c.reps FROM `exercise` AS e, `consistsof` AS c WHERE c.UserID = $id AND c.Workout_Id = '$value' AND c.exerciseId = e.ID";
+                    echo "<tr><td>Excercises</td><td>Targeted Body Part</td><td>Sets</td><td>Reps</td><td>Equipment</td></tr>";
+                    $sql = "SELECT e.name, e.bodyPart, e.equipment, c.sets, c.reps FROM `exercise` AS e, `consistsof` AS c WHERE c.UserID = $id AND c.Workout_Id = $w_id AND c.exerciseId = e.ID";
                     $result = mysqli_query($conn,$sql) or die("Bad Query: $sql");
                     while($row = mysqli_fetch_assoc($result)){
                           $name = $row['name'];
-                          $bodyPart = $row['bodyPart'];
+                          $part = $row['bodyPart'];
                           $sets = $row['sets'];
                           $reps = $row['reps'];
-                          echo "<tr><td>$name</td><td>$bodyPart</td><td>$sets</td><td>$reps</td></tr>";
+                          $equip = $row['equipment'];
+                          echo "<tr><td>$name</td><td>$part</td><td>$sets</td><td>$reps</td><td>$equip</td></tr>";
                     }
                     echo "</table>";
+					
+					//  add exercise //
+					echo "<form action='includes/routine_update.inc.php' method='POST'>
+							<input type='hidden' name='name' value='$value'>
+							<input type='hidden' name='w_id' value='$w_id'>
+							<input type='text' name='exercise' placeholder='Exercise Name'>
+							<input type='text' name='sets' placeholder='# of Sets'>
+							<input type='text' name='reps' placeholder='# of Reps'>
+							<button type='submit' name='submit_add'>Add to Routine</button>
+							</form>";
+					
+					//remove exercise//
+					echo "<form action='includes/routine_update.inc.php' method='POST'>
+							<input type='hidden' name='name' value='$value'>
+							<input type='hidden' name='w_id' value='$w_id'>
+							<input type='text' name='exercise' placeholder='Exercise Name'>
+							<button type='submit' name='submit_remove'>Remove from Routine</button>
+							</form>";
+					
+					/*  Search  by body part and/or equipment */
+					echo "<h2>_____________________________________________</h2>";
+					echo "<form action= 'includes/routine_update.inc.php' method='POST'>
+							<input type='text' name='bodypart' placeholder='Bodypart'>
+							<input type='text' name='equipment' placeholder='Equipment'>
+							<button type='submit' name='submit_search'>Search Exercises</button>
+							</form>";
+					
+					
+                    echo "<h2>_____________________________________________</h2>";
+                    echo "<form action='includes/routine_update.inc.php' method='POST'>
+                            <input type='hidden' name='name' value='$value'> 
+                            <button style='background:  #e74c3c  ;' button type='submit' name='submit_Delete'>DELETE ROUTINE</button>
+                            </form>";
+                     
+                     
                 }  
             }
-            else{
-				header("Location: index.php?error=notloggedin");
-                //echo '<h2>Please Login</h2>';
-            }
+            
         ?>
     </div>
 </section>
-
+ 
 <?php
     include_once 'footer.php';
 ?>
